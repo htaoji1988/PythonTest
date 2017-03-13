76fabc73f99cac4f12a4522dc25103045adb2545
tt <htaoji1988@126.com>
2017-03-13 14:07:32 +0800

    20170313

--- test.py
+++ test.py
@@ -1,10 +1,23 @@
-#!/usr/bin/python
-# coding=utf-8
+#!/usr/bin/pyhton
+account_file = 'user.txt'
+lock_file = 'lock.txt'
 
-a = 40  # 创建对象  <40>
-b = a  # 增加引用， <40> 的计数
-c = [b]  # 增加引用.  <40> 的计数
-
-del a  # 减少引用 <40> 的计数
-b = 100  # 减少引用 <40> 的计数
-c[0] = -1  # 减少引用 <40> 的计数
+for i in range(3):
+    username = raw_input('username:').strip()
+    password = raw_input('password:').strip()
+    if len(username) != 0 and len('password') != 0:
+        f = file(account_file)
+        login_status = False
+        for line in f.readlines():
+            line = line.split()
+            if username == line[0] and password == line[1]:
+                print 'hello welcome %s login!' % username
+                login_status = True
+        if login_status is True:
+            break
+    else:
+        continue
+else:
+    f = file(lock_file, 'a+')
+    f.write(username + '\n')
+    f.close()
