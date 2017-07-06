32dfd76c9c6f429d8928ce4fb08237e51e50184f
tt <htaoji1988@126.com>
2017-07-06 09:27:44 +0800

    20170705

--- test/DB_test.py
+++ test/DB_test.py
@@ -6,9 +6,24 @@ connection = pymysql.connect(host='172.16.107.136',
                              db='testdb',
                              charset='utf8mb4',
                              cursorclass=pymysql.cursors.DictCursor)
-# cur = conn.cursor()
-# cur.execute("SELECT * FROM user")
-# for r in cur.fetchall():
-#    print(r)
-# cur.close()
-# conn.close()
+cursor = connection.cursor()
+
+sql = "SELECT * FROM EMPLOYEE \
+       WHERE INCOME > '%d'" % (1000)
+
+try:
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    for row in results:
+        fname = row[0]
+        lname = row[1]
+        age = row[2]
+        sex = row[3]
+        income = row[4]
+
+        print("fname=%s,lname=%s,age=%d,sex=%s,income=%d" % \
+              (fname, lname, age, sex, income))
+except:
+    # 如果发生错误则回滚
+    print("Error: unable to fetch data")
+connection.close()
