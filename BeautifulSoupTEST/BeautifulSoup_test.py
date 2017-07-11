d5d5627cf3a5f63bf500a137a2f816bca60fbea3
tt <htaoji1988@126.com>
2017-07-11 17:49:55 +0800

    20170707

---
+++ BeautifulSoupTEST/BeautifulSoup_test.py
@@ -0,0 +1,8 @@
+from urllib import request
+from bs4 import BeautifulSoup
+
+
+content = request.urlopen('http://www.baidu.com')
+soup = BeautifulSoup(content, 'html.parser')
+
+print(soup.find('id="link3"'))
--- test/eq.py
+++ test/eq.py
@@ -34,7 +34,8 @@ def prettyprint(solution):
         return '. ' * (pos) + 'X ' + '. ' * (length - pos - 1)
 
     for pos in solution:
-        print line(pos)
+        print
+        line(pos)
 
 
 if __name__ == "__main__":
